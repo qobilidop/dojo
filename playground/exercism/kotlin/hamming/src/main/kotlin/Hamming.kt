d08ba1bb9c6b485b0497d91b20399ff09a53c3c2
qobilidop <qobilidop@gmail.com>
2020-07-13 11:17:49 +0800

    [exercism/kotlin] Finish hamming

---
+++ playground/exercism/kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -0,0 +1,9 @@
+object Hamming {
+
+    fun compute(leftStrand: String, rightStrand: String): Int {
+        if (leftStrand.length != rightStrand.length) {
+            throw IllegalArgumentException("left and right strands must be of equal length")
+        }
+        return (leftStrand.zip(rightStrand) { l, r -> if (l == r) 0 else 1}).sum()
+    }
+}

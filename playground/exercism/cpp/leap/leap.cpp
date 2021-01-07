4552872ed3776bf619ab0809e47c1606d3017a0b
qobilidop <qobilidop@gmail.com>
2021-01-07 12:19:52 -0800

    [exercism/cpp/leap] Refactor my solution

--- playground/exercism/cpp/leap/leap.cpp
+++ playground/exercism/cpp/leap/leap.cpp
@@ -6,19 +6,7 @@ bool is_leap_year(int year) {
     // on every year that is evenly divisible by 4
     //   except every year that is evenly divisible by 100
     //     unless the year is also evenly divisible by 400
-    if (year % 4 == 0) {
-        if (year % 100 == 0) {
-            if (year % 400 == 0) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return true;
-        }
-    } else {
-        return false;
-    }
+    return (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
 }
 
 }  // namespace leap

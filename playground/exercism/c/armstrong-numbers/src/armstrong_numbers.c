560c4207e711048ebf7e2a3b80ac28b7f55fc388
qobilidop <qobilidop@gmail.com>
2021-01-07 12:09:33 -0800

    [excercism/c/armstrong-numbers] Refactor solution

--- playground/exercism/c/armstrong-numbers/src/armstrong_numbers.c
+++ playground/exercism/c/armstrong-numbers/src/armstrong_numbers.c
@@ -1,20 +1,37 @@
-#include <math.h>
-
 #include "armstrong_numbers.h"
 
-int transform(int candidate)
+int count_digits(int number)
+{
+    int n_digit = 1;
+    while (number > 9) {
+        number /= 10;
+        n_digit++;
+    }
+    return n_digit;
+}
+
+int powi(int base, int exponent)
+{
+    int power = 1;
+    for (int i = 0; i < exponent; i++) {
+        power *= base;
+    }
+    return power;
+}
+
+int  armstrong_transform(int number)
 {
     int transformed = 0;
-    int n_digit = ceil(log10(candidate));
-    while (candidate > 0) {
-        int last_digit = candidate % 10;
-        transformed += pow(last_digit, n_digit);
-        candidate /= 10;
+    int n_digit = count_digits(number);
+    while (number > 0) {
+        int last_digit = number % 10;
+        transformed += powi(last_digit, n_digit);
+        number /= 10;
     }
     return transformed;
 }
 
 bool is_armstrong_number(int candidate)
 {
-    return candidate == transform(candidate);
+    return candidate == armstrong_transform(candidate);
 }
